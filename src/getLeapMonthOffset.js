ec5449cf96f522a39a3ff473ec8157f954f158d0
junryo <dangtrunganh@gmail.com>
2019-11-25 20:16:56 +0700

    + getLeapMonthOffset

---
+++ src/getLeapMonthOffset.js
@@ -0,0 +1,17 @@
+const getSunLongitude = require('./getSunLongitude')
+const getNewMoonDay = require('./getNewMoonDay')
+
+/* Find the index of the leap month after the month starting on the day a11. */
+module.exports = function(a11, timeZone) {
+	var k, last, arc, i;
+	k = INT((a11 - 2415021.076998695) / 29.530588853 + 0.5);
+	last = 0;
+	i = 1; // We start with the month following lunar month 11
+	arc = getSunLongitude(getNewMoonDay(k+i, timeZone), timeZone);
+	do {
+		last = arc;
+		i++;
+		arc = getSunLongitude(getNewMoonDay(k+i, timeZone), timeZone);
+	} while (arc != last && i < 14);
+	return i-1;
+}
